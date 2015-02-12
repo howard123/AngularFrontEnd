b6398b431423bb9da07884c1eb0247aecf812ed9
Howard <howards_s@yahoo.com>
2015-02-12 18:23:54 -0500

    commit on function calling internal REST service from Angular

--- AngularFrontEnd/WebContent/public/js/hello.js
+++ AngularFrontEnd/WebContent/public/js/hello.js
@@ -1,3 +1,15 @@
+
+
+
+
+function Account($scope, $http) {
+    $http.get('http://127.0.0.1:8080/account?emp_id=2').
+        success(function(data) {
+            $scope.account = data;
+        });
+}
+
+
 function Hello($scope, $http) {
     $http.get('http://rest-service.guides.spring.io/greeting').
         success(function(data) {
